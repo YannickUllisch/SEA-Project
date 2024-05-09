1dff7bb3e590a50e80fba4a5b243f9a593ac9a3b
Ertan Köse <erta0015@live.dk>
2024-05-09 14:10:31 +0200

    Demo rubn

--- renderer/pages/survey.tsx
+++ renderer/pages/survey.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import "survey-core/defaultV2.min.css";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+
+const surveyJson = {
+  elements: [
+    {
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text",
+    },
+    {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text",
+    },
+  ],
+};
+const survey = new Model(surveyJson);
 
 const NextPage = () => {
   return (
@@ -9,10 +28,11 @@ const NextPage = () => {
         <p>
           ⚡ Electron + Next.js ⚡ -<Link href="/">Go to home page</Link>
         </p>
+        <Survey model={survey} />
         <div> yoyoyo</div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default NextPage
+export default NextPage;
