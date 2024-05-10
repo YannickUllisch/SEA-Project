dcc2b77bc53c2cdc54b2691123630a281580fc8f
YannickUllisch <yannicku01@gmail.com>
2024-05-10 15:11:31 +0200

    added surveyjs libraries

--- renderer/pages/survey.tsx
+++ renderer/pages/survey.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
+import { Box } from '@mui/material'
+import 'survey-core/defaultV2.min.css'
+import { Model } from 'survey-core'
+import { Survey } from 'survey-react-ui'
 
 const NextPage = () => {
+  const surveyJson = {
+    elements: [
+      {
+        name: 'FirstName',
+        title: 'Enter your first name:',
+        type: 'text',
+      },
+      {
+        name: 'LastName',
+        title: 'Enter your last name:',
+        type: 'text',
+      },
+    ],
+  }
+
+  const survey = new Model(surveyJson)
+
   return (
-    <>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -<Link href="/">Go to home page</Link>
-        </p>
-        <div> yoyoyo</div>
-      </div>
-    </>
+    <Box
+      sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}
+    >
+      <Survey model={survey} />
+    </Box>
   )
 }
 
