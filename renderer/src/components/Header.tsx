62bccf55183f7e4389a3040be49b9801a445ec54
Sofie Xu <sofiexu@Sofies-MBP-2.lan>
2024-05-20 15:34:02 +0200

    Creator tab removed

--- renderer/src/components/Header.tsx
+++ renderer/src/components/Header.tsx
@@ -1,30 +1,30 @@
-import { Box, Button, Tooltip, Typography } from "@mui/material";
-import React from "react";
-import theme from "../lib/theme";
-import { useRouter } from "next/router";
-import { useSession } from "./SessionProvider";
-import { logout } from "@renderer/src/lib/logout";
-import { HomeIcon, Settings, LogOut } from "lucide-react";
-import Image from "next/image";
-import logo from "@renderer/public/images/logo.png";
-import { Role } from "@renderer/src/lib/role";
+import { Box, Button, Tooltip, Typography } from '@mui/material'
+import React from 'react'
+import theme from '../lib/theme'
+import { useRouter } from 'next/router'
+import { useSession } from './SessionProvider'
+import { logout } from '@renderer/src/lib/logout'
+import { HomeIcon, Settings, LogOut } from 'lucide-react'
+import Image from 'next/image'
+import logo from '@renderer/public/images/logo.png'
+import { Role } from '@renderer/src/lib/role'
 
 const Header = () => {
-  const router = useRouter();
-  const session = useSession();
+  const router = useRouter()
+  const session = useSession()
 
   return (
     <>
-      {router.pathname !== "/" && (
+      {router.pathname !== '/' && (
         <Box
           sx={{
-            height: "70px",
-            width: "100%",
-            display: "flex",
-            backgroundColor: "white",
-            margin: "0 auto",
-            justifyContent: "space-between",
-            alignItems: "center",
+            height: '70px',
+            width: '100%',
+            display: 'flex',
+            backgroundColor: 'white',
+            margin: '0 auto',
+            justifyContent: 'space-between',
+            alignItems: 'center',
           }}
         >
           <Box sx={{ ml: 3, mt: 1 }}>
@@ -33,33 +33,27 @@ const Header = () => {
           {session && (
             <Box>
               <Button
-                onClick={() => router.push("/admin")}
+                onClick={() => router.push('/admin')}
                 sx={{ color: theme.palette.text.secondary }}
               >
                 Home
-              </Button>
-              <Button
-                onClick={() => router.push("/admin/experiment/surveycreation")}
-                sx={{ color: theme.palette.text.secondary }}
-              >
-                Creator
               </Button>
             </Box>
           )}
           <Box>
             {session
               ? session.user.role === Role.OWNER && (
-                  <Tooltip title={"Settings"}>
+                  <Tooltip title={'Settings'}>
                     <Button
                       sx={{
                         color: theme.palette.text.secondary,
-                        "&:hover": {
-                          backgroundColor: "inherit",
-                          textDecoration: "underline",
+                        '&:hover': {
+                          backgroundColor: 'inherit',
+                          textDecoration: 'underline',
                         },
                       }}
                       variant="text"
-                      onClick={() => router.push("/settings")}
+                      onClick={() => router.push('/settings')}
                     >
                       <Settings />
                     </Button>
@@ -70,25 +64,25 @@ const Header = () => {
               <Button
                 sx={{
                   color: theme.palette.text.secondary,
-                  "&:hover": {
-                    backgroundColor: "inherit",
-                    textDecoration: "underline",
+                  '&:hover': {
+                    backgroundColor: 'inherit',
+                    textDecoration: 'underline',
                   },
                   gap: 0.5,
                 }}
                 variant="text"
-                onClick={() => router.push("/")}
+                onClick={() => router.push('/')}
               >
                 Login
               </Button>
             ) : (
-              <Tooltip title={"Logout"}>
+              <Tooltip title={'Logout'}>
                 <Button
                   sx={{
                     color: theme.palette.text.secondary,
-                    "&:hover": {
-                      backgroundColor: "inherit",
-                      textDecoration: "underline",
+                    '&:hover': {
+                      backgroundColor: 'inherit',
+                      textDecoration: 'underline',
                     },
                     ml: -2,
                     mr: 2,
@@ -104,7 +98,7 @@ const Header = () => {
         </Box>
       )}
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
