2a5a5e9f825a15047a65c16f08dccb05317dc557
Annika <annikasegal@gmail.com>
2024-05-20 15:28:56 +0200

    restart code - redirects to participant homepage

--- renderer/pages/participant.tsx
+++ renderer/pages/participant.tsx
@@ -1,41 +1,41 @@
-import React, { useEffect } from "react";
-import { Box, Button, Typography } from "@mui/material";
-import { useRouter } from "next/router";
-import theme from "@renderer/src/lib/theme";
-import { useSession } from "@renderer/src/components/SessionProvider";
-import { logout } from "@renderer/src/lib/logout";
+import React, { useEffect } from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+import theme from '@renderer/src/lib/theme'
+import { useSession } from '@renderer/src/components/SessionProvider'
+import { logout } from '@renderer/src/lib/logout'
 
 const ParticipantHomePage = () => {
-  const router = useRouter();
-  const session = useSession();
+  const router = useRouter()
+  const session = useSession()
 
   useEffect(() => {
     if (session) {
-      logout();
+      logout()
     }
-  }, [session]);
+  }, [session])
 
   return (
     <Box
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
       }}
     >
       <Box
         sx={{
-          backgroundColor: "white",
+          backgroundColor: 'white',
           padding: 5,
           marginTop: 10,
-          minWidth: "500px",
-          textAlign: "center",
+          minWidth: '500px',
+          textAlign: 'center',
           borderRadius: 4,
         }}
       >
         <Box sx={{ marginBottom: 3 }}>
-          <Typography variant="h4" fontWeight={"bold"}>
+          <Typography variant="h4" fontWeight={'bold'}>
             Questionnaire
           </Typography>
           <Typography color={theme.palette.text.secondary} variant="body2">
@@ -47,18 +47,18 @@ const ParticipantHomePage = () => {
           variant="contained"
           sx={{
             backgroundImage: `linear-gradient(to right, ${theme.palette.primary.dark}, ${theme.palette.secondary.light})`,
-            color: "white", // Text color
-            "&:hover": {
+            color: 'white', // Text color
+            '&:hover': {
               backgroundImage: `linear-gradient(to right, ${theme.palette.primary.dark}, ${theme.palette.secondary.main})`,
             },
           }}
-          onClick={() => router.push("/survey")}
+          onClick={() => router.push('/survey')}
         >
           Start Questionnaire
         </Button>
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default ParticipantHomePage;
+export default ParticipantHomePage
